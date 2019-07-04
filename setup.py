8cb3ff69028f688d891e02dfe8bc2eecdc97095f
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-04 17:49:51 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name='GF2Matrix',
     version='0.1',
     packages=['GF2Matrix'],
-    package_dir={'': 'GF2Matrix'},
+    #package_dir={'': 'GF2Matrix'},
     url='https://github.com/EwanGilligan/GF2Matrix',
     license='MIT',
     author='Ewan Gilligan',
     author_email='eg207@st-andrews.ac.uk',
     description='Cython implementation of a matrix with entries from GF(2).',
-    ext_modules=cythonize("int_matrix.pyx")
+    ext_modules=cythonize("GF2Matrix/int_matrix.pyx"),
+    include_dirs=[numpy.get_include()]
 )
